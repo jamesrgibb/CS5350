f8114c71116eeb6681f8569a312edaea8007e36d
James Gibb <51032141+jamesrgibb@users.noreply.github.com>
2023-11-07 20:05:35 -0700

    Perceptron

--- Perceptron/run.sh
+++ Perceptron/run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# The path to the Jupyter notebook
 NOTEBOOK_PATH="HW3.ipynb"
 
-# The directory where you want to save the output notebook
 OUTPUT_DIR="."
 
-# Run Jupyter Notebook
 jupyter nbconvert --to notebook --execute "$NOTEBOOK_PATH" --output "$OUTPUT_DIR/Executed_HW3.ipynb"
